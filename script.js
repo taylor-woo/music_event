640cfc82bbac7da5066ae2ba5a4844871210a24c
taylor-woo <taylorwoo05@gmail.com>
2025-08-25 19:18:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.botpressWebChat.init({
+    "composerPlaceholder": "Ask a question in English or Chinese",
+    "botConversationDescription": "This is a friendly chatbot",
+    "botId": "6372420d-7243-4cdf-b452-ee66e17a4e30",
+    "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+    "messagingUrl": "https://messaging.botpress.cloud",
+    "clientId": "a510af2e-04a1-4415-82d8-0bd32df00d5f",
+    "lazySocket": true,
+    "botName": "Event Assistant",
+    "stylesheet": "https://webchat-styler-widgets.botpress.app/prod/e925da49-2d68-451e-8f47-110006c6e7a6/v32535/style.css"
+});
